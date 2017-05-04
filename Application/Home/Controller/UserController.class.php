85a8e7908eab4ecfb87cfd964473a37436e0e34b
shirley7199 <617543072@qq.com>
2017-05-04 16:43:45 +0800

    565466546

--- Application/Home/Controller/UserController.class.php
+++ Application/Home/Controller/UserController.class.php
@@ -3,12 +3,8 @@ namespace Home\Controller;
 
 use Think\Controller;
 class UserController extends Controller{
-    private $UserModel;
     
-    public function __construct() {
-        parent::__construct();//子类继承父类
-        $this->UserModel= M("");
-    }
+     
 }
 
 ?>
--- ThinkPHP/Conf/convention.php
+++ ThinkPHP/Conf/convention.php
@@ -55,9 +55,9 @@ return  array(
     /* 数据库设置 */
     'DB_TYPE'               =>  'mysql',     // 数据库类型
     'DB_HOST'               =>  'localhost', // 服务器地址
-    'DB_NAME'               =>  'BM',          // 数据库名
+    'DB_NAME'               =>  'bm',          // 数据库名
     'DB_USER'               =>  'root',      // 用户名
-    'DB_PWD'                =>  '950104',          // 密码
+    'DB_PWD'                =>  '111',          // 密码
     'DB_PORT'               =>  '3306',        // 端口
     'DB_PREFIX'             =>  '',    // 数据库表前缀
     'DB_PARAMS'          	=>  array(), // 数据库连接参数    
